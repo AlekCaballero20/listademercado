c8647238f91919436b7d388edf3163cae0a3e2f4
AlekCaballero20 <alekcaballeromusic@gmail.com>
2026-03-02 21:47:20 -0500

    Delete js directory

--- js/js/app.js
+++
@@ -1,8 +0,0 @@
-import { registerPWA } from "./pwa.js";
-import { getDB } from "./state.js";
-import { bindActions } from "./ui.actions.js";
-import { renderAll } from "./ui.render.js";
-
-registerPWA();
-bindActions();
-renderAll(getDB());
